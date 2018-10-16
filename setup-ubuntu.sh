13285d10334dcbd798ceca8942e7f3117c69cd45
hall9000 <hall900@hackerspace.if.usp.br>
2018-10-16 15:05:06 -0300

    Primeiro commit

---
+++ setup-ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Programas para instalar
+# nmap, micro, git, anaconda, julia, gimp, processing, fritizing
+
+# Pacotes necessários
+sudo apt update && sudo apt install curl
+
+# PPAs
+## gimp
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+
+## vscode
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+
+## fritzing
+sudo add-apt-repository ppa:ehbello/fritzing
+
+# Atualização
+sudo apt update
+
+# Instalação
+sudo apt-get install nmap git gimp gimp-gmic gmic code fritzing fritzing-data fritzing-parts
+
+# snap
+snap install micro --classic
+
+# Instalar na mão: anaconda, julia, processing
+
